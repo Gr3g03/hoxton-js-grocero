139959ee21d529ec7ba6849b9dea81b4c6341882
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2021-12-02 15:59:02 +0100

    working with quantity

--- index.js
+++ index.js
@@ -1,59 +1,71 @@
 
+let cartListItems = []
+
 const state = {
   items: [
     {
   id: 1,
   name: 'beetroot',
-  price: 1 , 
+  price: 1 ,
+  quantity: 0
 },
 {
   id: 2,
   name: 'carrot',
-  price: 1
+  price: 1,
+  quantity: 0
 },
 {
   id: 3,
   name: 'apple',
-  price: 1
+  price: 1,
+  quantity: 0
 },
 {
   id: 4,
   name: 'apricot',
-  price: 1
+  price: 1,
+  quantity: 0
 },
 {
   id : 5,
   name: 'avocado',
-  price: 1
+  price: 1,
+  quantity: 0
 },
 {
   id: 6,
   name: 'bananas',
-  price: 1
+  price: 1,
+  quantity: 0
 },
 {
   id: 7,
   name: 'bell-pepper',
-  price: 1
+  price: 1,
+  quantity: 0
 },
 {
   id: 8,
   name: 'berry',
-  price: 1
+  price: 1,
+  quantity: 0
 },
 {
   id : 9,
   name: 'blueberry',
-  price: 1
+  price: 1,
+  quantity: 0
 },
 {
 id : 10, 
 name: 'eggplant',
-price: 1
+price: 1,
+quantity: 0
 }
-]
+],
+cartListItems: []
 }
-
 
 
 function addZeros(number) {
@@ -61,13 +73,29 @@ function addZeros(number) {
   const pad = "000";
   return pad.substring(0, pad.length - str.length) + str;
 }
+const totalProductSum = document.querySelector('.total-number')
 
-
-function addToCart(item){
-  state.items.filter(function (addItemsToCart){
-   return addItemsToCart.name === item.name
-  })
+function getSum(){
+  let  sum = 0
+  let price = state.cartListItems
+  for(const element of cartListItems){
+  sum = sum + element.price 
+  }
+  totalProductSum.textContent = `£${sum}`
+  // sum = sum + price
+  // totalProductSum.textContent = `£${sum}`
 }
+getSum()
+
+function increaseQuantity (){
+  cartListItems --
+}
+
+
+function decreaseQuantity (){
+  cartListItems ++
+}
+
 
 //* <ul class="item-list store--item-list"> */
 function storeItemList(){
@@ -86,8 +114,12 @@ storeimgEl.setAttribute('alt', `${item.name}`)
 const storeBnEl = document.createElement('button')
 storeBnEl.textContent = 'Add to cart'
 storeBnEl.addEventListener('click', function(){
-  addToCart(item);
-  render()
+  
+  if(item.quantity  <= 0){
+    cartListItems.push(state.items[(item.id)-1])
+    item.quantity++ }
+    else item.quantity--
+    cartItmelList(item)
 })
 
 storedivIconEl.append(storeimgEl)
@@ -97,10 +129,10 @@ itemListUlEl.append(storeLiEl)
 }
 
 //* <ul class="item-list cart--item-list"> */
-function cartItmelList(){
-  // for(const item of state.items){
-   const item = state.items 
-const itemListUlEl = document.querySelector('main .item-list')
+function cartItmelList(item){
+  const itemListUlEl = document.querySelector('main .item-list')
+  itemListUlEl.innerHTML = ``
+  for(const item of cartListItems){
 const cartLiEl = document.createElement('li')
 
 const imgCartEl = document.createElement('img')
@@ -109,6 +141,7 @@ imgCartEl.setAttribute('src', `assets/icons/${addZeros(item.id)}-${item.name}.sv
 imgCartEl.setAttribute('alt', `${item.name}`)
 
 const pEl = document.createElement  ('p')
+pEl.textContent =`${item.name}`
 
 const CartremoveBTnEl = document.createElement ('button')
 CartremoveBTnEl.setAttribute('class', 'quantity-btn remove-btn center')
@@ -122,11 +155,40 @@ const CartraddBTnEl = document.createElement ('button')
 CartraddBTnEl.setAttribute('class', 'quantity-btn add-btn center')
 CartraddBTnEl.textContent = '+'
 
+
 cartLiEl.append(imgCartEl, pEl, CartremoveBTnEl, CartSpanEl, CartraddBTnEl )
-itemListUlEl.append(cartLiEl)
+itemListUlEl.append(cartLiEl, totalProductSum)
   
+CartremoveBTnEl.addEventListener('click', function(){
+
+  const i =1
+  let spanNr = Number(CartSpanEl.textContent)
+  spanNr -= i
+  CartSpanEl.textContent = spanNr
+  if (spanNr === 0){
+    cartLiEl.remove()
+  }
+
+  // increaseQuantity ()
+  decreaseQuantity ()
+
+})
+CartraddBTnEl.addEventListener('click', function(){
+
+  // const i =1
+  // let spanNr = Number(CartSpanEl.textContent)
+  // spanNr += i
+  // CartSpanEl.textContent = spanNr
+
+  increaseQuantity ()
+  // decreaseQuantity ()
+})
+getSum()
+
 }
-// }
+}
+
+
 
 function render(){
   storeItemList()
